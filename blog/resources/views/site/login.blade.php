5b5b5fb21974402404e0488b85db8f38bbdb8b44
Luis <luis.oliveira@netscandigital.com.br>
2019-10-25 19:40:54 +0000

    layout login wip

--- blog/resources/views/site/login.blade.php
+++ blog/resources/views/site/login.blade.php
@@ -3,7 +3,7 @@
 @section('conteudo')
 <div class="flex justify-center mt-10">
 <form class="w-full max-w-sm">
-  <div class="md:flex md:items-center mb-6">
+  <div class="md:flex md:items-center mb-6 mt-24">
     <div class="md:w-1/3">
       <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
         Nome
@@ -23,8 +23,17 @@
       <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe">
     </div>
   </div>
+
+  <div class="md:flex md:items-center mb-6">
+
+  <div class="md:w-1/3">
+      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+        Email
+      </label>
+    </div>
+
     <div class="inline-block relative w-64">
-        <select name="UserType" @change="selectUserType()" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+        <select name="UserType" @change="selectUserType()" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline">
             <option value="0">Jogador</option>
             <option value="1">Administrador</option>
         </select>
@@ -32,7 +41,9 @@
             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
         </div>
     </div>
-    <span v-if="adm"> teste </span>
+
+  </div>
+
     <div class="container" v-if="adm">
       <div class="md:flex md:items-center mb-6">
       <div class="md:w-1/3">
